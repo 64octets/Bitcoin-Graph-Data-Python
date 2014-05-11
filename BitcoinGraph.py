df6d5ed8feea3b202c56918c66b950a95b024088
Kurush Dubash <dubash.kurush@gmail.com>
2014-05-11 09:35:40 -0700

    Added Percent complete

--- BitcoinGraph.py
+++ BitcoinGraph.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 import numpy as np
 import matplotlib.pyplot as plt
 from urllib.request import urlopen
-# from pylab import *
+
 def get_array_of_values(url):
 	array_of_values = []
 	for line in urlopen(url):
@@ -17,23 +17,32 @@ def getAverage(values):
 	for index in values:
 		total += index
 	return total / len(values)
+
+def percent(num, total):
+	return str(int((num/total) * 100)) +'%' 
+
 base_string = "https://coinbase.com/api/v1/prices/historical?page="
 strings = []
 data = []
+
 print("How many blocks to get info on? (per 1000)")
 inpu = int(input())
+
 print('Fetching data')
 for num in range(inpu):
 	strings.append(base_string + str(num))
+
 counter = 1
 for string in strings:
 	data.append(get_array_of_values(string))
-	print('Found 1000 blocks of data: (' + str(counter) +'/'+ str(inpu) + ')')
+	print('Found 1000 blocks of data: (' + str(counter) +'/'+ str(inpu) + ') ' + percent(counter, inpu))
 	counter += 1
 final_data = []
+
 print('Data imported successfully')
 for values in data:
 	final_data += values
+
 print('Plotting Data')	
 fig = plt.figure(figsize=(16, 5.5))
 fig.subplots_adjust(left=0.06)
